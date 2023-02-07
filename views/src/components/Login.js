ee5dafa7298c9ffd60fc9e20d546dab110a886e9
Pedro <peedrohs.mesquita@outlook.com>
2023-02-06 21:47:18 -0300

    CSS module changes

--- views/src/components/Login.js
+++ views/src/components/Login.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { postLogin } from '../services/UserServices';
-import '../styles/login.css';
+import LoginCSS from  '../styles/login.module.css';
 
-export default function Login({isDarkMode}) {
+export default function Login({isDarkMode, setRegistration}) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const inputClass = isDarkMode ? 'dark-mode' : '';
+    const inputClass = isDarkMode ? `${LoginCSS.inputField} ${LoginCSS.darkMode}` : `${LoginCSS.inputField}`;
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -14,12 +14,12 @@ export default function Login({isDarkMode}) {
     }
 
     return (
-        <div className='main-login'>
-            <div className={'card-login' + (isDarkMode ? ' dark-mode' : '')}>
+        <div className={LoginCSS.mainLogin}>
+            <div className={isDarkMode ? `${LoginCSS.cardLogin} ${LoginCSS.darkMode}` : `${LoginCSS.cardLogin}`}>
                 <h1>Entrar no Álbum</h1>
                 <form onSubmit={e => handleSubmit(e)}>
-                    <div className='textfield'>
-                        <label htmlFor='email'>E-mail</label>
+                    <div className={LoginCSS.textfield}>
+                        <label htmlFor='email' className={LoginCSS.labelField}>E-mail</label>
                         <input
                         type='email'
                         id='email'
@@ -30,8 +30,8 @@ export default function Login({isDarkMode}) {
                         onChange={e => setEmail(e.target.value)}
                         />
                     </div>
-                    <div className='textfield'>
-                        <label htmlFor='password'>Senha</label>
+                    <div className={LoginCSS.textfield}>
+                        <label htmlFor='password' className={LoginCSS.labelField}>Senha</label>
                         <input 
                         type='password'
                         id='password'
@@ -46,7 +46,7 @@ export default function Login({isDarkMode}) {
                     </div>
                     <button type="submit">Entrar</button>
                 </form>
-                <p>Não tem uma conta? <a href='#'>Crie uma aqui!</a></p>
+                <p>Não tem uma conta? <span onClick={() => setRegistration(true)} >Crie uma aqui!</span></p>
             </div>
         </div>
     );

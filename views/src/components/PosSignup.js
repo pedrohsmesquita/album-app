e82eadfe00b10a6f7d474513a555a6f5294461ec
Pedro <peedrohs.mesquita@outlook.com>
2023-02-12 23:23:59 -0300

    Pos signup changes

--- views/src/components/PosSignup.js
+++ views/src/components/PosSignup.js
@@ -6,7 +6,7 @@ export default function PosSignup({isDarkMode}) {
             <div className={isDarkMode ? `${style.card} ${style.darkMode}` : style.card}>
                 <h1>Confirmação de E-mail</h1>
                 <p className={style.mainText}>Uma mensagem com um link foi enviada para seu e-mail. Você tem até uma hora para clicar no link recebido e confirmar a criação de sua conta.<br/></p>
-                <p className={style.bottomText}><br/>Caso não apareca nada na caixa de entrada, cheque sua caixa de lixo ou spam.</p>
+                <p className={style.bottomText}><br/>Caso não apareca nada na caixa de entrada, cheque sua caixa de lixo ou spam.<br/>Você será redirecionado a página de login.</p>
             </div>
         </div>
     )

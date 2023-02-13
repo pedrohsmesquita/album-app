273e49f46b85f63d9280e8cc7db23bcd3d9c5d98
Pedro <peedrohs.mesquita@outlook.com>
2023-02-12 23:04:05 -0300

    JWT error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -6,6 +6,10 @@ const errorHandler = (err, req, res, next) => {
             message: err.message,
         });
     }
+    if (err.name === 'JsonWebTokenError')
+        return res.status(401).json({message: 'Token inválido'});
+    if (err.name === 'TokenExpiredError')
+        return res.status(401).json({message: 'O seu token expirou', code: 100});
     return res.status(500).json({message: 'Algo de errado ocorreu'});
 }
 

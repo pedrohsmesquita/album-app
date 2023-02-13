26b6f338dbe38233cb757b908f0b05f045bdfefb
Pedro <peedrohs.mesquita@outlook.com>
2023-02-12 23:03:21 -0300

    Authentication added

--- src/auth.js
+++ src/auth.js
@@ -8,7 +8,7 @@ exports.signup = async (req, res) => {
     const tempUserFound = await TempUser.findOne({email: req.body.email});
     const userFound = await User.findOne({email: req.body.email});
     if (tempUserFound || userFound)
-        throw new AppHandler(409, 'E-mail cadastrado');
+        throw new AppHandler(409, 'E-mail já cadastrado');
     const confirmationToken = jwt.sign({email: req.body.email}, process.env.CONFIRMATION_SECRET, { expiresIn: 3600 });
     const newUser = new TempUser({
         email: req.body.email,
@@ -16,23 +16,8 @@ exports.signup = async (req, res) => {
         password: req.body.password,
         confirmationCode: confirmationToken,
     });
-    const userSaved = await newUser.save();
-    const token  = jwt.sign({}, process.env.SECRET, {
-        subject: userSaved._id.toString(),
-        expiresIn: 60
-    });
-    const tokenSplit = token.split('.');
-    res.cookie('APP_CREDENTIALS', `${tokenSplit[0]}.${tokenSplit[1]}`, {
-        secure: process.env.NODE_ENV !== 'development',
-        maxAge: 1000 * 60 * 1,
-        sameSite: 'strict'
-    });
-    res.cookie('APP_SIGNATURE', tokenSplit[2], {
-        secure: process.env.NODE_ENV !== 'development',
-        httpOnly: true,
-        sameSite: 'strict'
-    });
-    await sendConfirmationEmail(userSaved.name, userSaved.email, userSaved.confirmationCode);
+    await newUser.save();
+    await sendConfirmationEmail(req.body.name, req.body.email, confirmationToken);
     return res.status(201).json({message: 'Usuário cadastrado com sucesso'});
 };
 
@@ -48,7 +33,41 @@ exports.verifyUser = async (req, res) => {
         images: [],
         createdAt: tempUser.createdAt
     });
-    const user = await newUser.save();
+    await newUser.save();
     await tempUser.delete();
     return res.status(200).json({message: 'E-mail confirmado'});
 };
+
+exports.login = async (req, res) => {
+    if (req.cookies.APP_CREDENTIALS && req.cookies.APP_SIGNATURE) {
+        jwt.verify(`${req.cookies.APP_CREDENTIALS}.${req.cookies.APP_SIGNATURE}`,
+                    process.env.SECRET, {active: true});
+        res.cookie('APP_CREDENTIALS', req.cookies.APP_CREDENTIALS, {
+            secure: process.env.NODE_ENV !== 'development',
+            maxAge: 1000 * 60 * 5,
+            sameSite: 'strict'
+        });
+        return res.status(200).json({message: 'Logado com sucesso'});
+    }
+    if (Object.keys(req.body).length === 0)
+        throw new AppHandler(401, 'É necessário as credenciais para fazer login');
+    const user = await User.findOne({email: req.body.email});
+    if (!user || !user.isValidPassword(req.body.password))
+        throw new AppHandler(401, 'E-mail ou senha incorreto');
+    const token = jwt.sign({active: true}, process.env.SECRET, {
+        subject: user._id.toString(),
+        expiresIn: 3600 * 24
+    });
+    const tokenSplit = token.split('.');
+    res.cookie('APP_CREDENTIALS', `${tokenSplit[0]}.${tokenSplit[1]}`, {
+        secure: process.env.NODE_ENV !== 'development',
+        maxAge: 1000 * 60 * 5,
+        sameSite: 'strict'
+    });
+    res.cookie('APP_SIGNATURE', tokenSplit[2], {
+        secure: process.env.NODE_ENV !== 'development',
+        httpOnly: true,
+        sameSite: 'strict'
+    });
+    return res.status(200).json({message: 'Logado com sucesso'});
+}
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
-const { signup, verifyUser } = require("./auth.js");
+const { signup, verifyUser, login } = require("./auth.js");
 const { tryCatch } = require("./utils/tryCatch.js");
 
 module.exports = (app) => {
-    app.post('/auth/signup', tryCatch(signup))
+    app.post('/auth/signup', tryCatch(signup));
 
     app.post('/auth/signup/confirm/:confirmationCode', tryCatch(verifyUser));
+
+    app.post('/auth/login', tryCatch(login));
 }

2116df557dc1297408c6330f51ce0b8de66dcc79
Pedro <peedrohs.mesquita@outlook.com>
2023-02-15 17:39:05 -0300

    App.js changed

--- views/src/App.js
+++ views/src/App.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login.js';
+import { tokenAuthentication } from './services/UserServices.js';
 import ConfirmationPage from './pages/ConfirmationPage.js';
-import Signup from './components/Signup.js';
 import NotFound from './components/NotFound.js';
+import Spinner from './components/Spinner.js';
+import Album from './pages/Album.js';
 
 export default function App() {
     const [isDarkMode, setIsDarkmode] = useState(false);
-    const [isRegistration, setIsRegistration] = useState(false);
+    const [status, setStatus] = useState('loading');
 
     useEffect(() => {
         window.matchMedia('(prefers-color-scheme: dark')
@@ -21,18 +22,29 @@ export default function App() {
         }
     }, []);
 
+    useEffect(() => {
+        const api = async () => {
+            const data = await tokenAuthentication();
+            if (data.status === 401)
+                setStatus('signin');
+            else
+                setStatus('authenticated');
+        }
+
+        api();
+    }, []);
+
     function onSelectTheme(isDarkTheme) {
         setIsDarkmode(isDarkTheme);
     }
 
+    if (status === 'loading')
+        return (<Spinner isDarkMode={isDarkMode}/>);
+
     return (
         <Router>
             <Routes>
-                <Route path='/' element={isRegistration ? (
-                    <Signup isDarkMode={isDarkMode} setRegistration={setIsRegistration}/>
-                ) : (
-                    <Login isDarkMode={isDarkMode} setRegistration={setIsRegistration}/>
-                )}/>
+                <Route path='/' element={<Album isDarkMode={isDarkMode} status={status} setStatus={setStatus}/>}/>
                 <Route path='/auth/signup/confirm/:confirmationCode' element={<ConfirmationPage isDarkMode={isDarkMode}/>}/>
                 <Route path='*' element={<NotFound />}/>
             </Routes>

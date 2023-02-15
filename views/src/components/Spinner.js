af5c105e503693c6e134a9249352cc1a03fe92e8
Pedro <peedrohs.mesquita@outlook.com>
2023-02-15 17:35:08 -0300

    Spinner changes

--- views/src/components/Spinner.js
+++ views/src/components/Spinner.js
@@ -1,8 +1,8 @@
 import style from '../styles/spinner.module.css';
 
-export default function Spinner() {
+export default function Spinner({isDarkMode}) {
     return (
-        <div className={style.loaderContainer}>
+        <div className={isDarkMode ? `${style.loaderContainer} ${style.darkMode}` : style.loaderContainer}>
             <div className={style.spinner}></div>
         </div>
     );

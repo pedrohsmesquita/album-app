c89d14e6912a34ea1948566c9cf5742c76f32c71
Pedro <peedrohs.mesquita@outlook.com>
2023-02-15 17:35:52 -0300

    API calls

--- views/src/services/UserServices.js
+++ views/src/services/UserServices.js
@@ -2,10 +2,20 @@ import axios from 'axios';
 
 export async function postSignup(data) {
     const response = await axios.post('/auth/signup', data, { validateStatus: false });
-    return {status: response.status, res: response.data}
+    return {status: response.status, res: response.data};
 }
 
 export async function verifyUser(confirmationCode) {
     const response = await axios.post('/auth/signup/confirm/' + confirmationCode, { validateStatus: false });
+    return {status: response.status, res: response.data};
+}
+
+export async function postLogin(data) {
+    const response = await axios.post('/auth/login', data, { validateStatus: false });
+    return {status: response.status, res: response.data};
+}
+
+export async function tokenAuthentication() {
+    const response = await axios.post('/auth/token', {}, { validateStatus: false });
     return {status: response.status, res: response.data};
 }
